5599ff86f8c5b4dd850a096427703a0226a9f73f
Manhnguyen015 <Manh.nguyen@student.oulu.fi>
2024-09-27 21:38:39 +0300

    Update app.js

--- Part4/app.js
+++ Part4/app.js
@@ -32,7 +32,7 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.use("/api/notes", blogsRouter);
+app.use("/", blogsRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandle);
